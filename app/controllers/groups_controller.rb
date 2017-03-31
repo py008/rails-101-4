f04471db68e13e564e3e71839487827afd934f68
庞羽 <py008@pyMacBook-Pro.local>
2017-03-31 13:08:15 +0800

    implement groups#update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -13,7 +13,7 @@ class GroupsController < ApplicationController
 
   def edit
     @group = Group.find(params[:id])
-  end  
+  end
 
   def create
     @group = Group.new(group_params)
@@ -24,6 +24,25 @@ class GroupsController < ApplicationController
       render :new
     end
   end
+
+  def update
+    @group = Group.find(params[:id])
+
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "Updated success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    redirect_to groups_path, alert: "Group.deleted"
+  end
+
+
+
 
   private
 
