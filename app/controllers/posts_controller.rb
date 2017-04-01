99a2007b83e31623bd3e42f8774b71ce3950f669
庞羽 <py008@pyMacBook-Pro.local>
2017-04-01 18:08:42 +0800

    implement my posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,29 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "Updated Success."
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to account_posts_path, alert: "Post Deleted"
+  end
+
 
   private
 
